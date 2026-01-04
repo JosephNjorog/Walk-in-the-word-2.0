269dffa9fd1ab51427609048297d08382b0e4ce2
JosephNjorog <njorojoe11173@gmail.com>
2026-01-04 10:25:23 +0300

    Upgrade: Textarea Import Error Fixed:

--- src/app/partnerships/page.tsx
+++ src/app/partnerships/page.tsx
@@ -7,6 +7,7 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import {
   ArrowLeft,
